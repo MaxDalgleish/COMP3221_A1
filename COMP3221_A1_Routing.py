f221de61741085ea3e28f6915f59b321982e5d5e
Max Dalgleish <maxdalgleish@gmail.com>
2024-03-06 17:38:25 +1100

    Added initial command line reading

---
+++ COMP3221_A1_Routing.py
@@ -0,0 +1,40 @@
+import networkx as nx
+import sys
+
+def main():
+	# Read the command line arguments <Node-ID> <Port-NO> <Node-ConfigFile>
+	args = sys.argv[1:]
+	if (len(args) != 3):
+		print("Usage: python COMP3221_A1_Routing.py <Node-ID> <Port-NO> <Node-ConfigFile>")
+		return
+
+	node_id, port_no, config_file = args[0], args[1], args[2]
+
+	# Ensure Node-ID is a english letter
+	if not node_id.isalpha() or not node_id.isupper() or len(node_id) != 1:
+		print("Error: Node-ID must be a single capital letter of the English alphabet")
+		return
+	
+	# Ensure Port-NO 
+	try:
+		port_no = int(port_no)
+		if port_no < 6000:
+			raise ValueError("Error: Port-NO must be greater than or equal to 6000")
+	except ValueError as e:
+		print(str(e))
+		return
+	
+
+	# Read the configuration file
+	try:
+		with open(config_file, "r") as f:
+			lines = f.readlines()
+	except FileNotFoundError:
+		print("Error: File not found")
+		return
+
+	return
+
+
+if __name__ == "__main__":
+	main()
