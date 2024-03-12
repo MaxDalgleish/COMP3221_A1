da13fae8237a445f45328a6b3bca3dee90e606bd
Max Dalgleish <maxdalgleish@gmail.com>
2024-03-12 15:29:13 +1100

    Added a base layer for the listening thread

--- COMP3221_A1_Routing.py
+++ COMP3221_A1_Routing.py
@@ -2,20 +2,68 @@ import networkx as nx
 import socket
 import threading
 import sys
+import time
+
+class ListeningThread(threading.Thread):
+	def __init__(self, node_id, port_no):
+		threading.Thread.__init__(self)
+		self.node_id = node_id
+		self.port_no = port_no
+	
+	def stop(self):
+		self._stop.set()
+	
+	def stopped(self):
+		return self._stop.is_set()
+
+	def run(self):
+		# Create a socket
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind(('localhost', self.port_no))
+		s.listen(9)
+
+		while True:
+			if self.stopped():
+				print(self.name + " is stopping")
+				s.close()
+				return
+			try:
+				time.sleep(self.processing_time)
+			except:
+				print("Alreay interrupted")
+			# Accept the incoming connection
+			conn, addr = s.accept()
+			print("Connection from: ", addr)
+
+			# Receive the data
+			data = conn.recv(1024)
+			if not data:
+				break
+			print("Received: ", data.decode("utf-8"))
+
+			# Send the data back to the client
+			conn.close()
 
 def main():
+	# Check if the input is valid
+	if not valid_input_check():
+		return
+
+
+
+def valid_input_check():
 	# Read the command line arguments <Node-ID> <Port-NO> <Node-ConfigFile>
 	args = sys.argv[1:]
 	if (len(args) != 3):
 		print("Usage: python COMP3221_A1_Routing.py <Node-ID> <Port-NO> <Node-ConfigFile>")
-		return
+		return False
 
 	node_id, port_no, config_file = args[0], args[1], args[2]
 
 	# Ensure Node-ID is a english letter
 	if not node_id.isalpha() or not node_id.isupper() or len(node_id) != 1:
 		print("Error: Node-ID must be a single capital letter of the English alphabet")
-		return
+		return False
 	
 	# Ensure Port-NO 
 	try:
@@ -24,7 +72,7 @@ def main():
 			raise ValueError("Error: Port-NO must be greater than or equal to 6000")
 	except ValueError as e: 
 		print(str(e))
-		return
+		return False
 	
 
 	# Read the configuration file
@@ -33,11 +81,9 @@ def main():
 			lines = f.readlines()
 	except FileNotFoundError:
 		print("Error: File not found")
-		return
+		return False
 
-
-	return
-
+	return True
 
 if __name__ == "__main__":
 	main()
