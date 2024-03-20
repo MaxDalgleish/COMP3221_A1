6cdf1ce98eaac206ea9fd7dff542666e036a5345
Max Dalgleish <maxdalgleish@gmail.com>
2024-03-20 15:05:25 +1100

    Updated routing

--- test1.sh
+++ test1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 COMP3221_A1_Routing.py A 6000 Config_Files/Xconfig.txt &
+(python3 COMP3221_A1_Routing.py A 6000 Config_Files/Xconfig.txt > a.out) &
 PIDA=$!
-python3 COMP3221_A1_Routing.py D 6003 Config_Files/Yconfig.txt &
+(python3 COMP3221_A1_Routing.py D 6003 Config_Files/Yconfig.txt > d.out) &
 PIDD=$!
-python3 COMP3221_A1_Routing.py G 6006 Config_Files/Zconfig.txt &
+(python3 COMP3221_A1_Routing.py G 6006 Config_Files/Zconfig.txt > g.out) &
 PIDG=$!
 
 sleep 30
